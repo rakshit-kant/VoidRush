78febf8b4870e27f5bca6dcabdaf13aa8fb4714b
rakshit-kant <rakshitkant100@gmail..com>
2026-09-06 19:38:12 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use bevy::{input::keyboard::Key, prelude::*};
+
+// Represents the Playable Spaceship
+#[derive(Component)]
+struct Spaceship;
+
+// Defines the Velocity of the Asteroids
+// #[derive(Component)]
+// struct Velocity(Vec2);
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawns the 2D Camera
+    commands.spawn(Camera2d);
+
+    // Initializes What Spaceship and Velocity are, And Spawns the Spaceship Sprite on the Screen
+    // Idk what the Transform thingy does
+    commands.spawn((
+        Spaceship,
+        // Velocity(Vec2::ZERO),
+        Sprite {
+            image: asset_server.load("spaceship.png"),
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        Sprite {
+            image: asset_server.load("background.png"),
+            ..default()
+        },
+        Transform::from_scale(Vec3::splat(2.0)),
+    )); // OMG! Look at these Brackets
+}
+
+// Exits the Application if Escape Key is pressed as I am too lazy to touch my Mouse... One of the
+// Reasons I use Lazyvim
+fn exit_on_esc(keyboard_input: Res<ButtonInput<Key>>, mut app_exit_events: MessageWriter<AppExit>) {
+    if keyboard_input.just_pressed(Key::Escape) {
+        app_exit_events.write(AppExit::Success);
+    }
+}
+
+fn main() {
+    // If you need to Read this... You are lazier than me as You can't read the Above Comments
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, exit_on_esc)
+        .add_systems(Startup, setup)
+        .run();
+}
