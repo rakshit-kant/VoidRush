150b2a4e38f907c3b876fbac2e3177ff366a20a1
rakshit-kant <rakshitkant100@gmail.com>
2026-09-18 16:12:22 +0530

    Fixed Capitalization Errors

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,88 @@
-use bevy::{input::keyboard::key, prelude::*};
+use bevy::{input::keyboard::KeyCode, prelude::*};
 
 // represents the playable spaceship
-#[derive(component)]
-struct spaceship;
+#[derive(Component)]
+struct Spaceship;
 
 // defines the velocity of the asteroids
-// #[derive(component)]
-// struct velocity(vec2);
+// #[derive(Component)]
+// struct Velocity(Vec2);
 
-fn setup(mut commands: commands, asset_server: res<assetserver>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // spawns the 2d camera
-    commands.spawn(camera2d);
+    commands.spawn(Camera2d);
 
     // spawn background
     commands.spawn((
-        sprite {
+        Sprite {
             image: asset_server.load("background.png"),
             ..default()
         },
-        transform::from_xyz(0.0, 0.0, -1.0), // kept behind the spaceship via z axis
+        Transform::from_xyz(0.0, 0.0, -1.0), // kept behind the spaceship via z axis
     ));
 
     // spawns spaceship
     commands.spawn((
-        spaceship,
-        // velocity(vec2::zero),
-        sprite {
+        Spaceship,
+        // velocity(Vec2::ZERO),
+        Sprite {
             image: asset_server.load("spaceship.png"),
             ..default()
         },
         // idk what this does, so apparently chatgpt told me that it just gets multiplied to the xyz
         // values like x * splat or something like that
-        transform::from_xyz(0.0, -250.0, 0.0).with_scale(vec3::splat(2.0)),
+        Transform::from_xyz(0.0, -250.0, 0.0).with_scale(Vec3::splat(2.0)),
     )); // omg! look at these brackets
 }
 
-fn move_spaceship() {
-    const spaceship_speed: f32 = 400.0;
+fn move_spaceship(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut spaceship: Single<&mut Transform, With<Spaceship>>,
+    time: Res<Time>,
+) {
+    const SPACESHIP_SPEED: f32 = 400.0;
+
+    let mut direction = Vec2::ZERO;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        direction.x -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        direction.x += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        direction.y += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowDown) {
+        direction.y -= 1.0;
+    }
+
+    if direction != Vec2::ZERO {
+        direction = direction.normalize();
+    }
+
+    spaceship.translation += direction.extend(0.0) * SPACESHIP_SPEED * time.delta_secs();
 }
 
 // exits the application if escape key is pressed as i am too lazy to touch my mouse... one of the
 // reasons i use lazyvim
-fn exit_on_esc(keyboard_input: res<buttoninput<key>>, mut app_exit_events: messagewriter<appexit>) {
-    if keyboard_input.just_pressed(key::escape) {
-        app_exit_events.write(appexit::success);
+fn exit_on_esc(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut app_exit_events: MessageWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_events.write(AppExit::Success);
     }
 }
 
 fn main() {
     // if you need to read this... you are lazier than me as you can't read the above comments
-    app::new()
-        .add_plugins(defaultplugins)
-        .add_systems(update, exit_on_esc)
-        .add_systems(startup, setup)
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, (move_spaceship, exit_on_esc))
+        .add_systems(Startup, setup)
         .run();
 }
